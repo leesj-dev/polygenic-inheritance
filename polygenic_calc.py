972ee7e90026ae48f3e9c26d07ec6e82b0fedae1
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2022-10-20 23:50:30 +0900

    Update polygenic_calc.py

--- polygenic_calc.py
+++ polygenic_calc.py
@@ -19,25 +19,19 @@ def get_ratio(poly_list):
 
 def get_possible_capital(linkage):  # [2, 0] 꼴
     possible_capital = list(range(0, linkage + 1))
-    capital_set = []
-    for i in range(0, len(possible_capital)):
-        for j in range(0, len(possible_capital)):
-            if possible_capital[i] >= possible_capital[j]:
-                capital_set.append(tuple([possible_capital[i], possible_capital[j]]))
-
+    rng = range(0, len(possible_capital))
+    capital_set = [(possible_capital[i], possible_capital[j]) for i in rng for j in rng if possible_capital[i] >= possible_capital[j]] 
+    
     return capital_set
 
 
 alleles = 5
 linkages_list = [list(x) for splits in range(1, alleles + 1) for x in comb(range(1, alleles + 1), splits) if sum(x) == alleles]
-
 final_result = []
-k_values = {}
 linkages_result = {}
 
 for k in range(0, len(linkages_list)):
     linkages = linkages_list[k]
-    possible_capital_list = []
     k_values = defaultdict(list)
     possible_capital_list = [get_possible_capital(capital_set) for capital_set in linkages]
 
@@ -53,7 +47,6 @@ for item in final_result:
     print(item)
 
 print("\n")
-
 
 for key, value in linkages_result.items():
     link_status = sorted([x.strip() for x in str(key)[1:-1].split(",")], reverse=True)
